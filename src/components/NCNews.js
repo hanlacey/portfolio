9540318bd77126f8650c06b685174b1c8697f94f
hanlacey <hnnhlcy@gmail.com>
2021-03-28 20:08:08 +0100

    update project component

--- src/components/NCNews.js
+++ src/components/NCNews.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function NCNews(props) {
     return (
-        <a className="project-title" href="https://hannah-nc-news.herokuapp.com/api"
-        >NC News backend Project</a>
+        <div className="project">
+            <a className="project-title" href="https://hannah-nc-news.herokuapp.com/api"
+            >NC News API</a>
+
+        </div>
 
     );
 }
