677e5fb000752d99b9df638f08bfef99611910a8
hanlacey <hnnhlcy@gmail.com>
2021-03-28 19:03:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          NC News backend project
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
